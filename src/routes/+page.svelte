2d08b5fcf683f41a2174984b8cdeed9b391d71f0
kampsyy <kampambachanda@gmail.com>
2024-07-09 11:49:19 +0200

    Add home page hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,33 @@
-<h1 class="text-2xl">Welcome to your library project</h1>
-<p class="border border-light-bor-active text-light-txt-secondary">Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p class="bg-light-bg-high_ctr">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Aside from '$lib/../docs/ui/aside.svelte';
+	import Row from '$lib/../docs/ui/row.svelte';
+	import Shell from '$lib/../docs/ui/shell.svelte';
+	import { asideData } from '$lib/../docs/utils/data.js';
+
+	
+</script>
+
+{#snippet foundation()}
+	<Row>
+		<h1
+			class="first-letter:capitalize text-light-gray-1000 dark:text-dark-gray-1000 text-[24px] lg:text-[40px] font-semibold leading-[32px] lg:leading-[48px] tracking-[-0.96px] lg:tracking-[-2.4px] mb-3"
+		>
+        Kodo Design System
+		</h1>
+		<p
+			class="first-letter:capitalize text-light-gray-900 dark:text-dark-gray-900 text-[16px] lg:text-[20px] font-normal leading-[24px] lg:leading-[30px] tracking-normal lg:tracking-[-0.33px]"
+		>
+        A Svelte 5 design system inspired by Vercel Geist, crafted to create consistent and cohesive web experiences.
+		</p>
+	</Row>
+{/snippet}
+
+{#snippet cont()}
+	{@render foundation()}
+{/snippet}
+
+{#snippet aside()}
+	<Aside asideDataList={asideData} />
+{/snippet}
+
+<Shell asideSlot={aside} contSlot={cont} />
