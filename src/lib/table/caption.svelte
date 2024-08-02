c8147eb64bf1ebe7873af154f233f504252ed025
kampsyy <kampambachanda@gmail.com>
2024-08-02 23:36:45 +0200

    add table caption

---
+++ src/lib/table/caption.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type propsT = {
+		class?: string | undefined;
+		children: Snippet | undefined;
+	};
+	let { class: klass = '', children = undefined }: propsT = $props();
+</script>
+
+<caption class="{klass}">
+	{#if children}
+		{@render children()}
+	{/if}
+</caption>
