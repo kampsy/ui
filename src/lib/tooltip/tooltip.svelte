98fa3191ff68f4274c5552f1a126e30c88bcba8e
kampsyy <kampambachanda@gmail.com>
2024-08-02 18:26:13 +0200

    update tooltip: set width based on text width in px

--- src/lib/tooltip/tooltip.svelte
+++ src/lib/tooltip/tooltip.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { getStringWidth } from '$lib/utils/text.js';
 	import type { Snippet } from 'svelte';
 
 	type propsT = {
@@ -7,26 +9,36 @@
 		type?: 'success' | 'error' | 'warning' | 'violet' | undefined;
 		children?: Snippet;
 	};
-
 	let { position = 'top', text, type = undefined, children = undefined }: propsT = $props();
 
+	let widthClass = $derived.by(() => {
+		if (text) {
+			const num = getStringWidth(text);
+			if (num < 250) {
+				console.log(text, num);
+				return 'width:' + num + 'px';
+			}
+		}
+		return 'width:250px';
+	});
+
 	const positionObj = {
-		top: `max-w-[250px] bottom-[125%] left-[50%] translate-x-[-50%] text-dark-gray-1000 
+		top: `bottom-[125%] left-[50%] translate-x-[-50%] text-dark-gray-1000 
 				dark:text-light-gray-1000 bg-light-gray-1000 dark:bg-dark-gray-1000 after:content-[' ']
 				after:absolute after:top-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px]
 				after:border-t-light-gray-1000 after:dark:border-t-dark-gray-1000 after:border-b-transparent
 				after:border-x-transparent `,
-		bottom: `max-w-[250px] top-[125%] left-[50%] translate-x-[-50%] text-dark-gray-1000 
+		bottom: `top-[125%] left-[50%] translate-x-[-50%] text-dark-gray-1000 
 				dark:text-light-gray-1000 bg-light-gray-1000 dark:bg-dark-gray-1000 after:content-[' ']
 				after:absolute after:bottom-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px] 
 				after:border-t-transparent after:border-b-light-gray-1000 after:dark:border-b-dark-gray-1000 
 				after:border-x-transparent `,
-		left: `max-w-[250px] top-[50%] right-[125%] transform translate-y-[-50%] text-dark-gray-1000 
+		left: `top-[50%] right-[125%] transform translate-y-[-50%] text-dark-gray-1000 
 				dark:text-light-gray-1000 bg-light-gray-1000 dark:bg-dark-gray-1000 after:content-[' ']
 				after:absolute after:top-[50%] after:left-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-light-gray-1000
 				after:dark:border-l-dark-gray-1000 after:border-r-transparent`,
-		right: `max-w-[250px] top-[50%] left-[125%] transform translate-y-[-50%] text-dark-gray-1000 
+		right: `top-[50%] left-[125%] transform translate-y-[-50%] text-dark-gray-1000 
 				dark:text-light-gray-1000 bg-light-gray-1000 dark:bg-dark-gray-1000 after:content-[' '] 
 				after:absolute after:top-[50%] after:right-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-transparent after:border-r-light-gray-1000
@@ -38,88 +50,88 @@
 
 	const typeObj = {
 		success: {
-			top: `max-w-[250px] bottom-[125%] left-[50%] translate-x-[-50%] text-light-bg 
+			top: `bottom-[125%] left-[50%] translate-x-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-blue-700 dark:bg-dark-blue-700 after:content-[' ']
 				after:absolute after:top-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px]
 				after:border-t-light-blue-700 after:dark:border-t-dark-blue-700 after:border-b-transparent
 				after:border-x-transparent `,
-			bottom: `max-w-[250px] top-[125%] left-[50%] translate-x-[-50%] text-light-bg 
+			bottom: `top-[125%] left-[50%] translate-x-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-blue-700 dark:bg-dark-blue-700 after:content-[' ']
 				after:absolute after:bottom-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px] 
 				after:border-t-transparent after:border-b-light-blue-700 after:dark:border-b-dark-blue-700 
 				after:border-x-transparent `,
-			left: `max-w-[250px] top-[50%] right-[125%] transform translate-y-[-50%] text-light-bg 
+			left: `top-[50%] right-[125%] transform translate-y-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-blue-700 dark:bg-dark-blue-700 after:content-[' ']
 				after:absolute after:top-[50%] after:left-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-light-blue-700
 				after:dark:border-l-dark-blue-700 after:border-r-transparent`,
-			right: `max-w-[250px] top-[50%] left-[125%] transform translate-y-[-50%] text-light-bg 
+			right: `top-[50%] left-[125%] transform translate-y-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-blue-700 dark:bg-dark-blue-700 after:content-[' '] 
 				after:absolute after:top-[50%] after:right-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-transparent after:border-r-light-blue-700
 				after:dark:border-r-dark-blue-700`
 		},
 		error: {
-			top: `max-w-[250px] bottom-[125%] left-[50%] translate-x-[-50%] text-light-bg 
+			top: `bottom-[125%] left-[50%] translate-x-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-red-700 dark:bg-dark-red-700 after:content-[' ']
 				after:absolute after:top-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px]
 				after:border-t-light-red-700 after:dark:border-t-dark-red-700 after:border-b-transparent
 				after:border-x-transparent `,
-			bottom: `max-w-[250px] top-[125%] left-[50%] translate-x-[-50%] text-light-bg 
+			bottom: `top-[125%] left-[50%] translate-x-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-red-700 dark:bg-dark-red-700 after:content-[' ']
 				after:absolute after:bottom-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px] 
 				after:border-t-transparent after:border-b-light-red-700 after:dark:border-b-dark-red-700 
 				after:border-x-transparent `,
-			left: `max-w-[250px] top-[50%] right-[125%] transform translate-y-[-50%] text-light-bg 
+			left: `top-[50%] right-[125%] transform translate-y-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-red-700 dark:bg-dark-red-700 after:content-[' ']
 				after:absolute after:top-[50%] after:left-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-light-red-700
 				after:dark:border-l-dark-red-700 after:border-r-transparent`,
-			right: `max-w-[250px] top-[50%] left-[125%] transform translate-y-[-50%] text-light-bg 
+			right: `top-[50%] left-[125%] transform translate-y-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-red-700 dark:bg-dark-red-700 after:content-[' '] 
 				after:absolute after:top-[50%] after:right-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-transparent after:border-r-light-red-700
 				after:dark:border-r-dark-red-700`
 		},
 		warning: {
-			top: `max-w-[250px] bottom-[125%] left-[50%] translate-x-[-50%] text-light-black 
+			top: `bottom-[125%] left-[50%] translate-x-[-50%] text-light-black 
 				dark:text-black bg-light-amber-700 dark:bg-dark-amber-700 after:content-[' ']
 				after:absolute after:top-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px]
 				after:border-t-light-amber-700 after:dark:border-t-dark-amber-700 after:border-b-transparent
 				after:border-x-transparent `,
-			bottom: `max-w-[250px] top-[125%] left-[50%] translate-x-[-50%] text-light-black 
+			bottom: `top-[125%] left-[50%] translate-x-[-50%] text-light-black 
 				dark:text-black bg-light-amber-700 dark:bg-dark-amber-700 after:content-[' ']
 				after:absolute after:bottom-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px] 
 				after:border-t-transparent after:border-b-light-amber-700 after:dark:border-b-dark-amber-700 
 				after:border-x-transparent `,
-			left: `max-w-[250px] top-[50%] right-[125%] transform translate-y-[-50%] text-light-black 
+			left: `top-[50%] right-[125%] transform translate-y-[-50%] text-light-black 
 				dark:text-black bg-light-amber-700 dark:bg-dark-amber-700 after:content-[' ']
 				after:absolute after:top-[50%] after:left-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-light-amber-700
 				after:dark:border-l-dark-amber-700 after:border-r-transparent`,
-			right: `max-w-[250px] top-[50%] left-[125%] transform translate-y-[-50%] text-light-black 
+			right: `top-[50%] left-[125%] transform translate-y-[-50%] text-light-black 
 				dark:text-black bg-light-amber-700 dark:bg-dark-amber-700 after:content-[' '] 
 				after:absolute after:top-[50%] after:right-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-transparent after:border-r-light-amber-700
 				after:dark:border-r-dark-amber-700`
 		},
 		violet: {
-			top: `max-w-[250px] bottom-[125%] left-[50%] translate-x-[-50%] text-light-bg 
+			top: `bottom-[125%] left-[50%] translate-x-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-purple-700 dark:bg-dark-purple-700 after:content-[' ']
 				after:absolute after:top-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px]
 				after:border-t-light-purple-700 after:dark:border-t-dark-purple-700 after:border-b-transparent
 				after:border-x-transparent `,
-			bottom: `max-w-[250px] top-[125%] left-[50%] translate-x-[-50%] text-light-bg 
+			bottom: `top-[125%] left-[50%] translate-x-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-purple-700 dark:bg-dark-purple-700 after:content-[' ']
 				after:absolute after:bottom-[100%] after:left-[50%] after:ml-[-5px] after:border-[5px] 
 				after:border-t-transparent after:border-b-light-purple-700 after:dark:border-b-dark-purple-700 
 				after:border-x-transparent `,
-			left: `max-w-[250px] top-[50%] right-[125%] transform translate-y-[-50%] text-light-bg 
+			left: `top-[50%] right-[125%] transform translate-y-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-purple-700 dark:bg-dark-purple-700 after:content-[' ']
 				after:absolute after:top-[50%] after:left-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-light-purple-700
 				after:dark:border-l-dark-purple-700 after:border-r-transparent`,
-			right: `max-w-[250px] top-[50%] left-[125%] transform translate-y-[-50%] text-light-bg 
+			right: `top-[50%] left-[125%] transform translate-y-[-50%] text-light-bg 
 				dark:text-light-bg bg-light-purple-700 dark:bg-dark-purple-700 after:content-[' '] 
 				after:absolute after:top-[50%] after:right-[100%] after:mt-[-5px] after:border-[5px] 
 				after:border-y-transparent after:border-l-transparent after:border-r-light-purple-700
@@ -147,7 +159,8 @@
 			{@render children()}
 		{/if}
 		<div
-			class="invisible group-hover:visible absolute {tooltipStyle} whitespace-nowrap px-3 py-2 text-xs text-center rounded-[4px] z-[1000]"
+			style={widthClass}
+			class="invisible group-hover:visible absolute {tooltipStyle} px-3 py-2 text-xs text-center rounded-[4px] z-[1000]"
 		>
 			{text || ''}
 		</div>
---
+++ src/lib/utils/text.ts
@@ -0,0 +1,30 @@
+import { browser } from "$app/environment";
+
+/**
+ * Calculates the width of a string in pixels using a specified font.
+ *
+ * @param {string} text - The text to measure the width of.
+ * @param {string} [font='12px Inter'] - The font to use for measuring the width. Defaults to '12px Inter'.
+ * @return {number} The width of the text in pixels. Returns 250 if context or the browser is not available.
+ */
+
+export const getStringWidth = (text: string, font = '12px Inter'): number => {
+    if (browser) {
+        // Create a temporary canvas element
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+
+        if (context) {
+            // Set the font to the context, this must match the font used in the UI
+            context.font = font;
+
+            // Measure the width of the text
+            const width = context.measureText(text).width;
+
+            const numStr = width.toFixed(0);
+            return Number(numStr);
+        }
+        return 250;
+    }
+    return 250;
+};
