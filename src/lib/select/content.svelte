35e3213406eba03c9bbfdfc14ae18b18e19cd5a2
kampsyy <kampambachanda@gmail.com>
2024-09-02 06:23:33 +0200

    update select. add mobile view

--- src/lib/select/content.svelte
+++ src/lib/select/content.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Button } from '$lib/index.js';
 	import { getContext, type Snippet } from 'svelte';
 	import type { Writable } from 'svelte/store';
 	import { fly } from 'svelte/transition';
@@ -10,23 +11,57 @@
 	let { class: klass = '', children }: propsT = $props();
 
 	// Get the select stores from the context
-	const { isActive, transY, contentPosition } = getContext<{
+	const { isMobile, isActive, transY, contentPosition } = getContext<{
+		isMobile: Writable<boolean>;
 		isActive: Writable<boolean>;
 		transY: Writable<number>;
 		contentPosition: Writable<string>;
 	}>('select');
 </script>
 
-{#if $isActive}
-	<div
-		in:fly={{ y: $transY }}
-		out:fly={{ y: $transY }}
-		class="absolute w-full {$contentPosition} z-[1000] {klass}"
-	>
+{#snippet mobileSnip()}
+	{#if $isActive}
 		<div
-			class="hide-scrollbar bg-kui-light-bg dark:bg-kui-dark-bg p-1 rounded-[6px] border border-kui-light-gray-200 dark:border-kui-dark-gray-400 shadow-sm scroll-smooth overflow-y-auto  {klass}"
+			in:fly|local={{ y: '100vh', duration: 500, opacity: 1 }}
+			out:fly|local={{ y: '100vh', duration: 500, opacity: 1 }}
+			class="fixed bottom-0 left-0 w-full rounded-t-[15px] bg-kui-light-bg-secondary dark:bg-kui-dark-bg-secondary lg:bg-transparent z-[1001]"
 		>
-			{@render children()}
+			<div
+				class="hide-scrollbar bg-kui-light-bg dark:bg-kui-dark-bg p-6 rounded-t-[15px] border-y border-kui-light-gray-400 dark:border-kui-dark-gray-400 scroll-smooth overflow-y-auto"
+			>
+				{@render children()}
+			</div>
+			<footer class="p-4">
+				<Button
+					onclick={() => {
+						$isActive = false;
+					}}
+					type="secondary"
+					class="w-full">done</Button
+				>
+			</footer>
 		</div>
-	</div>
+	{/if}
+{/snippet}
+
+{#snippet desktopSnip()}
+	{#if $isActive}
+		<div
+			in:fly={{ y: $transY }}
+			out:fly={{ y: $transY }}
+			class="absolute w-full {$contentPosition} z-[1000] {klass}"
+		>
+			<div
+				class="hide-scrollbar bg-kui-light-bg dark:bg-kui-dark-bg p-1 rounded-[6px] border border-kui-light-gray-200 dark:border-kui-dark-gray-400 shadow-sm scroll-smooth overflow-y-auto {klass}"
+			>
+				{@render children()}
+			</div>
+		</div>
+	{/if}
+{/snippet}
+
+{#if $isMobile}
+	{@render mobileSnip()}
+{:else}
+	{@render desktopSnip()}
 {/if}
--- src/lib/select/root.svelte
+++ src/lib/select/root.svelte
@@ -2,6 +2,7 @@
 	import { clickOutside } from '$lib/utils/event.js';
 	import { setContext, type Snippet } from 'svelte';
 	import { writable } from 'svelte/store';
+	import { fade } from 'svelte/transition';
 
 	type propsT = {
 		value?: string;
@@ -11,23 +12,53 @@
 	};
 	let { value = $bindable(''), size = 'medium', class: klass = '', children }: propsT = $props();
 
+
+	const isMobile = writable(false);
 	const selected = writable('');
 	const isActive = writable(false);
 	const transY = writable(-10);
 	const contentPosition = writable('top-[112%]');
+	
 	setContext('select', {
+		isMobile,
 		selected,
 		size: size,
 		isActive,
 		transY,
-		contentPosition
+		contentPosition,
 	});
 
 	// Assign the selected value to the parent component value prop when changed.
 	$effect(() => {
 		value = $selected;
 	});
+
+	$effect(() => {
+		if (window.innerWidth < 767) {
+			$isMobile = true;
+		} else {
+			$isMobile = false;
+		}
+		// update when the user is resizing the window
+		window.addEventListener('resize', () => {
+			if (window.innerWidth < 767) {
+				$isMobile = true;
+			} else {
+				$isMobile = false;
+			}
+		});
+	});
+
 </script>
+
+<!--Backgrop background on mobile only-->
+{#if $isActive}
+	<div
+		in:fade|local
+		out:fade|local
+		class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-[0.4] lg:hidden z-[1000]"
+	></div>
+{/if}
 
 <div use:clickOutside={() => ($isActive = false)} class="relative inline-block {klass}">
 	{@render children()}
