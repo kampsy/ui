2e8ae62cfd01f79d3582f2b7832be566a58b8d0c
kampsyy <kampambachanda@gmail.com>
2024-09-03 08:15:06 +0200

    Add meta social tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,37 @@
 </script>
 
 <svelte:head>
-    <title>Kampsy-ui</title>
+	<!-- Primary Meta Tags -->
+	<title>Kampsy UI</title>
+	<meta name="title" content="Kampsy-ui | A Svelte 5 component library" />
+	<meta
+	  name="description"
+	  content="Kampsy-ui - A Svelte 5 component library, thoughtfully designed to deliver consistent and cohesive web experiences." />
+  
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://ui.kampsy.xyz" />
+	<meta property="og:title" content="Kampsy-ui | A Svelte 5 component library" />
+	<meta
+	  property="og:description"
+	  content="Kampsy-ui - A Svelte 5 component library, thoughtfully designed to deliver consistent and cohesive web experiences." />
+	<meta
+	  property="og:image"
+	  content="https://ucarecdn.com/83b4db17-b2fe-4c66-8b7e-354151eb436d/-/preview/1200x628/" />
+  
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://ui.kampsy.xyz" />
+	<meta property="twitter:title" content="Kampsy-ui | A Svelte 5 component library" />
+	<meta
+	  property="twitter:description"
+	  content="Kampsy-ui - A Svelte 5 component library, thoughtfully designed to deliver consistent and cohesive web experiences." />
+	<meta
+	  property="twitter:image"
+	  content="https://ucarecdn.com/83b4db17-b2fe-4c66-8b7e-354151eb436d/-/preview/1200x628/" />
+  
+	<!-- Meta Tags Generated with https://metatags.io -->
+	<meta name="robots" content="noindex,nofollow" />
 </svelte:head>
 
 {#snippet foundation()}
