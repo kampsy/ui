d549773f139806bab9c8733cdaad8ab4821b015e
kampsyy <kampambachanda@gmail.com>
2024-09-06 08:11:55 +0200

    update spinner add custom size

--- src/routes/spinner/+page.svelte
+++ src/routes/spinner/+page.svelte
@@ -65,7 +65,7 @@
 		<h2
 			class="first-letter:capitalize text-kui-light-gray-1000 dark:text-kui-dark-gray-1000 text-[24px] font-semibold leading-[32px] tracking-[-0.96px] mb-3"
 		>
-			<a href="#customelabel" id="default">custome label</a>
+			<a href="#customsize" id="default">custom size</a>
 		</h2>
 		<div class="mt-4 xl:mt-7">
 			{#snippet demo()}
