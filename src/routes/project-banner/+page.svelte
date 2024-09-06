b052e725806d4f31c38c6674a4f500b550e346de
kampsyy <kampambachanda@gmail.com>
2024-09-06 08:37:17 +0200

    update project banner collapse code

--- src/routes/project-banner/+page.svelte
+++ src/routes/project-banner/+page.svelte
@@ -38,14 +38,16 @@
 
 {#snippet demoAndCode(demo: Snippet, code: string)}
 	<div
-		class="bg-kui-light-bg dark:bg-kui-dark-bg border border-kui-light-gray-200 dark:border-kui-dark-gray-400 rounded-xl overflow-hidden"
+		class="bg-kui-light-bg dark:bg-kui-dark-bg border border-kui-light-gray-200 dark:border-kui-dark-gray-400 rounded-xl"
 	>
-		<div class="w-full p-4 lg:p-6 overflow-x-auto">
-			<div class="w-full flex flex-wrap gap-4">
+		<div class="w-full p-4 lg:p-6">
+			<div class="w-full flex flex-nowrap gap-4 items-center justify-between">
 				{@render demo()}
 			</div>
 		</div>
-		<CollapseCode {code} />
+		<div class="overflow-hidden rounded-b-xl">
+			<CollapseCode {code} />
+		</div>
 	</div>
 {/snippet}
 
@@ -68,7 +70,7 @@
 			@johnphamous
 		</Tooltip>
 	{/snippet}
-	
+
 	<Row>
 		<h2
 			class="first-letter:capitalize text-kui-light-gray-1000 dark:text-kui-dark-gray-1000 text-[24px] font-semibold leading-[32px] tracking-[-0.96px] mb-3"
