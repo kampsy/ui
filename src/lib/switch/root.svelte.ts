ec4793186292155ee668730aa4de2e14e4a18d31
kampsyy <kampambachanda@gmail.com>
2024-09-26 19:49:54 +0200

    update switch code docs

--- src/lib/switch/root.svelte.ts
+++ src/lib/switch/root.svelte.ts
@@ -1,3 +1,14 @@
+
+/**
+ * Creates a state manager for a switch component.
+ *
+ * @param {Object} initial - The initial state of the switch component.
+ * @param {string} initial.selected - The initially selected value of the switch component.
+ * @param {string} initial.name - The name of the switch component.
+ * @param {('small' | 'medium' | 'large')} initial.size - The size of the switch component.
+ * @param {boolean} initial.fullWidth - Whether the switch component should take up the full width of its container.
+ * @returns {Object} An object with methods to get and set the selected value of the switch component.
+ */
 export function createRootState(initial: {
     selected: string,
     name: string,
