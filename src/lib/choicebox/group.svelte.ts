c491636e04d8b5ec5f36f5e58b17a6904f445806
kampsyy <kampambachanda@gmail.com>
2024-10-04 10:54:39 +0200

    chore:(choicebox) add docs to reative state

--- src/lib/choicebox/group.svelte.ts
+++ src/lib/choicebox/group.svelte.ts
@@ -1,4 +1,16 @@
 
+/**
+ * Creates a state manager for a Choicebox.Group component.
+ *
+ * The state manager is stored in the global state with the key 'choicebox'.
+ *
+ * @param {Object} initial the initial state of the Choicebox.Group
+ * @param {string | Array<string>} initial.selected the initially selected value of the Choicebox.Group
+ * @param {string} initial.name the name of the Choicebox.Group
+ * @param {'radio' | 'checkbox'} initial.type the type of the Choicebox.Group
+ * @param {boolean} initial.disabledParent whether the Choicebox.Group is disabled
+ * @returns {Object} an object with a getter and setter for the selected value of the Choicebox.Group
+ */
 export function createGroupState(initial: {
     selected: string | Array<string>, name: string,
     type: 'radio' | 'checkbox', disabledParent: boolean
