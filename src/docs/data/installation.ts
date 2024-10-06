17d84e608c7666c6f56d1bdfc30476e7a8bd7a13
Luca <luca.bosin@heviat.com>
2024-10-06 18:53:58 +0200

    Update documentation to include minimal and extended snippets

--- src/docs/data/installation.ts
+++ src/docs/data/installation.ts
@@ -16,6 +16,15 @@ import kampsyUI from 'kampsy-ui/preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  presets: [kampsyUI]
+}`;
+
+export const installationConfigLong = `
+import kampsyUI from 'kampsy-ui/preset';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: kampsyUI.content,
   presets: [kampsyUI],
   theme: {
     extend: {}
