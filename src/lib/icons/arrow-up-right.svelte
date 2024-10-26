da80f40904056b0a6917299d407d04d82c919f2c
kampsyy <kampambachanda@gmail.com>
2024-10-26 05:06:57 +0200

    chore: :bento: add icons

---
+++ src/lib/icons/arrow-up-right.svelte
@@ -0,0 +1,14 @@
+<svg
+	data-testid="geist-icon"
+	height="100%"
+	stroke-linejoin="round"
+	viewBox="0 0 16 16"
+	width="100%"
+	style="color: currentcolor;"
+	><path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M5.75001 2H5.00001V3.5H5.75001H11.4393L2.21968 12.7197L1.68935 13.25L2.75001 14.3107L3.28034 13.7803L12.4988 4.56182V10.25V11H13.9988V10.25V3C13.9988 2.44772 13.5511 2 12.9988 2H5.75001Z"
+		fill="currentColor"
+	></path></svg
+>
