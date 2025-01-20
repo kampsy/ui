1a4233a50f73746daf041ad87462af1cc683f035
kampsyy <kampambachanda@gmail.com>
2025-01-20 16:53:48 +0200

    fix: :bug: fix collapse ChevronDownSmall icon on old browsers

--- src/lib/collapse/trigger.svelte
+++ src/lib/collapse/trigger.svelte
@@ -57,15 +57,22 @@
 	};
 </script>
 
-<button {onclick} class="w-full flex items-center justify-between text-left bg-transparent {paddingClass} ">
+<button
+	{onclick}
+	class="w-full flex items-center justify-between text-left bg-transparent {paddingClass} "
+>
 	{#if children}
 		<span class="{textClass} text-kui-light-gray-1000 dark:text-kui-dark-gray-1000 font-semibold"
 			>{@render children()}</span
 		>
 	{/if}
 	<div class="">
-		<div class="w-[16px] h-[16px] {rotate} transform-gpu duration-200 text-kui-light-gray-1000 dark:text-kui-dark-gray-1000">
-			<ChevronDownSmall />
+		<div class="w-[16px] h-[16px] overflow-hidden">
+			<div
+				class="w-[16px] h-[16px] {rotate} transform-gpu duration-200 text-kui-light-gray-1000 dark:text-kui-dark-gray-1000"
+			>
+				<ChevronDownSmall />
+			</div>
 		</div>
 	</div>
 </button>
