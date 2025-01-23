388dafd9fb07fc1bfe48e0464816e8e2f4634cd5
kampsyy <kampambachanda@gmail.com>
2025-01-23 22:48:14 +0200

    feat: :zap: add transition on hover with a 300 duration

--- src/lib/showMore/showMore.svelte
+++ src/lib/showMore/showMore.svelte
@@ -1,52 +1,63 @@
 <script lang="ts">
 	import { ChevronDownSmall } from '$lib/icons/index.js';
 
-    type propsT = {
-        isActive: boolean;
-    }
+	type propsT = {
+		isActive: boolean;
+	};
 
-    let { isActive = $bindable(false) }: propsT = $props();
+	let { isActive = $bindable(false) }: propsT = $props();
 
+    const onclick = () => {
+		isActive = !isActive;
+	};
 
-    let rotate = $derived.by(() => {
+	let rotate = $derived.by(() => {
 		if (isActive) {
 			return 'rotate-180';
 		}
 		return '';
 	});
 
-    let buttonText = $derived.by(() => {
-        if (isActive) {
-            return 'show less';
-        }
-        return 'Show more';
-    })
+	let buttonText = $derived.by(() => {
+		if (isActive) {
+			return 'show less';
+		}
+		return 'Show more';
+	});
+    
+
 </script>
 
 {#snippet suffixSnip()}
-	<div class="w-4 h-4 overflow-hidden">
-		<div class="w-4 h-4 {rotate} transform-gpu duration-200 flex items-center justify-center">
-            <ChevronDownSmall/>
-        </div>
+	<div class="w-4 h-4">
+		<div
+			class="rounded-full w-4 h-4 {rotate} transform-gpu duration-200 flex items-center justify-center"
+		>
+			<ChevronDownSmall />
+		</div>
 	</div>
 {/snippet}
 
 <div class="w-full">
-	<div class="flex items-center">
-		<div class="w-full border-t border-kui-light-gray-400 dark:border-kui-dark-gray-400">
+	<div class="flex items-center box-border">
+		<div class="grow border-t border-kui-light-gray-400 dark:border-kui-dark-gray-400">
 			<!--line-->
-		</div>  
-            <div class="w-[250px]">
-                <button onclick="{()=>{
-                    isActive = !isActive
-                }}" type="button" class="w-full rounded-full border border-kui-light-gray-400 dark:border-kui-dark-gray-400">
-                    <div class="w-full px-2 py-2 flex items-center justify-between flex-nowrap gap-[8px]">
-                        <div class="font-medium first-letter:capitalize text-xs"> {buttonText}</div>
-                        {@render suffixSnip()}
-                    </div>
-                </button>
-            </div>
-		<div class="w-full border-t border-kui-light-gray-400 dark:border-kui-dark-gray-400">
+		</div>
+		<div class="grow-0">
+			<button
+				{onclick}
+				type="button"
+				class="rounded-full p-1.5 border border-kui-light-gray-400 dark:border-kui-dark-gray-400 
+                hover:border-kui-light-gray-500 dark:hover:border-kui-dark-gray-500 hover:bg-kui-light-gray-200 
+                dark:hover:bg-kui-dark-gray-200 transition duration-300"
+			>
+				<div class="px-1.5 w-full flex items-center gap-1 justify-center">
+					<div class="font-medium capitalize text-sm">{buttonText}</div>
+					{@render suffixSnip()}
+				</div>
+			</button>
+		</div>
+		<div class="grow border-t border-kui-light-gray-400 dark:border-kui-dark-gray-400">
 			<!--second line-->
 		</div>
 	</div>
