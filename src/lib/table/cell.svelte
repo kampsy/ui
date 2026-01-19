f4fa165b2e71437f1686e312673104b00d20ce50
kampsy <kampambachanda@gmail.com>
2026-01-19 14:59:11 +0200

    Update src/lib/table/cell.svelte
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/lib/table/cell.svelte
+++ src/lib/table/cell.svelte
@@ -8,7 +8,7 @@
 	let { class: klass = '', children = undefined }: Props = $props();
 </script>
 
-<td class="px-2 py-2.5 align-middle last:text-right [&:has([data-cell-link=true])]:p-0 [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-[2px] {klass}">
+<td class="px-2 py-2.5 align-middle last:text-right [&:has([data-cell-link=true])]:p-0 [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] {klass}">
 	{#if children}
 		{@render children()}
 	{/if}
