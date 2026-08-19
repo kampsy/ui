99151622ffd3ffc39517ab6bf976768a6cc118cc
kampsy <kampambachanda@gmail.com>
2026-08-19 17:14:15 +0200

    refactor(search-input): remove unused clear button and associated logic

--- src/lib/input/searchInput.svelte
+++ src/lib/input/searchInput.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Cross from "$lib/icons/cross.svelte"
 	import MagnifyingGlass from "$lib/icons/magnifying-glass.svelte"
 	import SearchInputSpinner from "./search-input-spinner.svelte"
 	import Input from "./input.svelte"
@@ -21,10 +20,6 @@
 		if (event.key === "Escape") value = ""
 		onkeydown?.(event)
 	}
-
-	function clearValue() {
-		value = ""
-	}
 </script>
 
 <div class="relative w-full" data-cmdk={cmdk ? "" : undefined}>
@@ -34,19 +29,7 @@
 		prefix={activePrefix}
 		prefixStyling={false}
 		aria-busy={loading ? true : undefined}
-		class={[klass, "[&>div>div>input]:pr-10"]}
+		class={klass}
 		onkeydown={handleKeydown}
 	/>
-
-	{#if value && !loading}
-		<button
-			class="text-kui-light-gray-700 dark:text-kui-dark-gray-700 hover:text-kui-light-gray-1000 dark:hover:text-kui-dark-gray-1000 absolute top-1/2 right-2 flex size-7 -translate-y-1/2 items-center justify-center rounded-full transition-colors"
-			type="button"
-			aria-label="Clear search"
-			disabled={rest.disabled}
-			onclick={clearValue}
-		>
-			<span class="size-4" aria-hidden="true"><Cross /></span>
-		</button>
-	{/if}
 </div>
